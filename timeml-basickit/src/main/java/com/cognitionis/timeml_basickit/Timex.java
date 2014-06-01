85bb3e8df4560803c3ea7bfe7686af9e6033a080
Hector Llorens <hectorlm1983@gmail.com>
2014-06-01 21:15:48 +0200

    fix

--- timeml-basickit/src/main/java/com/cognitionis/timeml_basickit/Timex.java
+++ timeml-basickit/src/main/java/com/cognitionis/timeml_basickit/Timex.java
@@ -19,7 +19,6 @@ public final class Timex extends Element implements Cloneable {
     public static final String lower_bound = "0000-01-01T00:00:00";
     private String type;
     private String value;
-    private String isoclean;
     private String lower_value;
     private String upper_value;
     private String granularity; // todo
@@ -48,11 +47,10 @@ public final class Timex extends Element implements Cloneable {
             // ERA solves the AD (Anno Domini)/CE (Common or Current Era) and  BC(before Christ)/BCE (before Common Era)
             //calendar.set(Calendar.ERA, GregorianCalendar.BC);
             // BY NOW: NO YEARS DIFFERENT OF 4 DIGITS
-
-            if (value.matches("[0-9]([0-9]([0-9]([0-9](-[0-9]{2}(-[0-9]{2}(T[0-2][0-9](:[0-5][0-9](:[0-5][0-9])?)?)?)?)?)?)?)?")) {
+            if (is_ISO8601_date(value)) {
                 initialize_interval_values();
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-                date = df.parse(this.isoclean);
+                date = df.parse(this.lower_value);
                 //DateTimeFormatter fmt = ISODateTimeFormat.dateOptionalTimeParser();
                 //date = new DateTime(fmt.parseDateTime(cleanvalue)).toDate();
             }
@@ -127,9 +125,8 @@ public final class Timex extends Element implements Cloneable {
         try {
             if (this.lower_value == null || this.upper_value == null) {
                 String[] interval_values_arr = get_interval_value(this.value).split("\\|");
-                this.isoclean = interval_values_arr[0];
-                this.lower_value = interval_values_arr[1];
-                this.upper_value = interval_values_arr[2];
+                this.lower_value = interval_values_arr[0];
+                this.upper_value = interval_values_arr[1];
             }
         } catch (Exception e) {
             System.err.println("Errors found (" + this.getClass().getSimpleName() + "):\n\t" + e.toString() + "\n");
@@ -152,17 +149,23 @@ public final class Timex extends Element implements Cloneable {
             if (value == null) {
                 return null;
             }
+            String lv = value;
+            String uv = value;
             // malformed T with - (-T)
             value = value.replaceAll("-T", "T");
             // one digit hours
             if (value.matches(".*T[0-9]:.*")) {
                 value = value.replaceAll("(.*)T(.*)", "$1T0$2");
             }
-
-            String isoclean = value;
-            String lv = value;
-            String uv = value;
+            // add 0 to <4 dates
             String year = "0000";
+            if (value.matches("^[0-9]+$")) {
+                if (value.length() > 4) {
+                    // error year above 9999
+                    throw new Exception("Year above 9999. Omitted.");
+                }
+            }
+            
             if (value.matches("^[0-9]{4}")) {
                 year = value.substring(0, 4);
             }
@@ -172,16 +175,16 @@ public final class Timex extends Element implements Cloneable {
                     if (isLeapYear(Integer.parseInt(year))) {
                         february_last_day = "29";
                     }
-                    return year + "-01|" + year + "-01-01|" + year + "-02-" + february_last_day;
+                    return  year + "-01-01|" + year + "-02-" + february_last_day;
                 }
                 if (value.matches("(?i).*-(SP|Q2|T2)")) {
-                    return year + "-03|" + year + "-03-01|" + year + "-05-31";
+                    return  year + "-03-01|" + year + "-05-31";
                 }
                 if (value.matches("(?i).*-(SU|Q3|H2|T3)")) {
-                    return year + "-06|" + year + "-06-01|" + year + "-08-31";
+                    return year + "-06-01|" + year + "-08-31";
                 }
                 if (value.matches("(?i).*-(AU|FA|Q4|T4)")) {
-                    return year + "-09|" + year + "-09-01|" + year + "-12-31";
+                    return year + "-09-01|" + year + "-12-31";
                 }
             }
 
@@ -189,22 +192,22 @@ public final class Timex extends Element implements Cloneable {
                 String date_string = value.substring(0, 10);
                 // MORNING 5-12
                 if (value.matches("(?i).*TMO")) {
-                    return date_string + "T05:00:00|" + date_string + "T05:00:00|" + date_string + "T11:59:59";
+                    return  date_string + "T05:00:00|" + date_string + "T11:59:59";
                 } // NOON 12
                 if (value.matches("(?i).*TNO")) {
-                    return date_string + "T12:00:00|" + date_string + "T12:00:00|" + date_string + "T12:00:01";
+                    return date_string + "T12:00:00|" + date_string + "T12:00:01";
                 } // AFTERNOON 13
                 if (value.matches("(?i).*TAF")) {
-                    return date_string + "T12:00:01|" + date_string + "T12:00:01|" + date_string + "T17:59:59";
+                    return date_string + "T12:00:01|" + date_string + "T17:59:59";
                 } // DEPEND ON WORK BREAKS 17-18
                 if (value.matches("(?i).*TEV")) {
-                    return date_string + "T18:00:00|" + date_string + "T18:00:00|" + date_string + "T20:59:59";
+                    return date_string + "T18:00:00|" + date_string + "T20:59:59";
                 } // AFTER WORK... GOING BACK HOME...
                 if (value.matches("(?i).*TNI")) {
-                    return date_string + "T21:00:00|" + date_string + "T21:00:00|" + date_string + "T23:59:59";
+                    return date_string + "T21:00:00|" + date_string + "T23:59:59";
                 } // MIDNIGHT
                 if (value.matches("(?i).*TMI")) {
-                    return date_string + "T00:00:00|" + date_string + "T00:00:00|" + date_string + "T00:00:01";
+                    return date_string + "T00:00:00|" + date_string + "T00:00:01";
                 }
             }
 
@@ -215,18 +218,18 @@ public final class Timex extends Element implements Cloneable {
                 cal.setTime(dateaux);
                 SimpleDateFormat df2 = new SimpleDateFormat(granul_days);
                 cal.add(GregorianCalendar.WEEK_OF_YEAR, Integer.parseInt(value.substring(6, 8)));
-                isoclean = df2.format(cal.getTime());
-                lv = isoclean + "T00:00:00";
+                lv = df2.format(cal.getTime()) + "T00:00:00";
                 cal.add(GregorianCalendar.DAY_OF_MONTH, 6);
                 uv = df2.format(cal.getTime()) + "T23:59:59";
                 if (value.endsWith("WE")) {
                     cal.add(GregorianCalendar.DAY_OF_MONTH, -1);
                     lv = df2.format(cal.getTime()) + "T00:00:00";
                 }
-                return isoclean + "|" + lv + "|" + uv;
+                return  lv + "|" + uv;
             }
 
-            if (value.matches("[0-9]([0-9]([0-9]([0-9](-[0-9]{2}(-[0-9]{2}(T[0-2][0-9](:[0-5][0-9](:[0-5][0-9])?)?)?)?)?)?)?)?")) {
+            //months and days and normal times (clean)
+            if (is_clean_ISO8601_date_part(value)) {
                 lv = value + lower_bound.substring(value.length());
                 uv = value + upper_bound.substring(value.length());
                 if (value.length() == 7) { // months
@@ -242,7 +245,7 @@ public final class Timex extends Element implements Cloneable {
             }
 
 
-            return isoclean + "|" + lv + "|" + uv;
+            return  lv + "|" + uv;
         } catch (Exception e) {
             System.err.println("Errors found (Timex):\n\t" + e.toString() + "\n");
             if (System.getProperty("DEBUG") != null && System.getProperty("DEBUG").equalsIgnoreCase("true")) {
@@ -267,8 +270,37 @@ public final class Timex extends Element implements Cloneable {
         return this.upper_value;
     }
 
+    
+    
     /**
-     * Simple function to calculate if a given year is a lap year
+     * True if the string is a valid ISO8601
+     *
+     * @param value
+     * @return
+     */
+    static boolean is_ISO8601_date(String value) {
+        if(value.matches("[0-9]([0-9]([0-9]([0-9](-[0-9]{2}(-[0-9]{2}(  T([0-2][0-9](:[0-5][0-9](:[0-5][0-9])?)? | (MO|AF|EV|NI|MI|NO) )  )?|-W[0-5][0-9](-WE)?)|-(WI|SP|SU|AU|FA|[QT](1|2|3|4)|H(1|2)))?)?)?)?")){
+            return true;
+        }
+        return false;
+    }    
+
+    /**
+     * True if the string is a valid clean ISO8601 (yyyy-MM-dd'T'HH:mm:ss)
+     *
+     * @param value
+     * @return
+     */
+    static boolean is_clean_ISO8601_date_part(String value) {
+        if(value.matches("[0-9]([0-9]([0-9]([0-9](-[0-9]{2}(-[0-9]{2}(T[0-2][0-9](:[0-5][0-9](:[0-5][0-9])?)?)?)?)?)?)?)?")){
+            return true;
+        }
+        return false;
+    }    
+    
+    
+    /**
+     * True if a given year is a lap year
      *
      * @param year
      * @return
